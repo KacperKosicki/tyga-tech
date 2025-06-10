e9b3c75a9a4bcac61ce2183b542f7182557f1946
Kacper Kosicki <kosickikacper1@gmail.com>
2025-06-11 00:22:04 +0200

    Dodanie AOS'a do AboutUs.

--- frontend/src/pages/AboutUs/AboutUs.jsx
+++ frontend/src/pages/AboutUs/AboutUs.jsx
@@ -4,16 +4,20 @@ import styles from './AboutUs.module.scss';
 const AboutUs = () => {
   return (
     <section className={styles.aboutUs}>
-      <div className={styles.bgImageWrapper}>
+      <div
+        className={styles.bgImageWrapper}
+        data-aos="fade-left"
+        data-aos-delay="100"
+      >
         <img src="/images/undraw-team-spirit.png" alt="" className={styles.bgImage} />
       </div>
 
       <div className={styles.container}>
-        <p>
+        <p data-aos="fade-up" data-aos-delay="0">
           <strong>TYGA‑TECH Sp. z o.o.</strong> to dynamicznie rozwijająca się firma z siedzibą w Krzyżu Wielkopolskim, specjalizująca się w serwisie, naprawach i instalacjach maszyn przemysłowych. Naszym celem jest dostarczanie niezawodnych i nowoczesnych rozwiązań technicznych dla klientów z całej Polski.
         </p>
 
-        <ul>
+        <ul data-aos="fade-up" data-aos-delay="100">
           <li>Naprawa, konserwacja i montaż urządzeń przemysłowych</li>
           <li>Produkcja konstrukcji i maszyn metalowych na zamówienie</li>
           <li>Obróbka mechaniczna elementów metalowych</li>
@@ -21,14 +25,14 @@ const AboutUs = () => {
           <li>Sprzedaż części zamiennych i akcesoriów</li>
         </ul>
 
-        <div className={styles.team}>
+        <div className={styles.team} data-aos="fade-up" data-aos-delay="200">
           <h3>Zarząd firmy</h3>
           <div className={styles.members}>
-            <div className={styles.member}>
+            <div className={styles.member} data-aos="fade-up" data-aos-delay="300">
               <h4>Krzysztof Remigiusz Wiza</h4>
               <p>Prezes Zarządu</p>
             </div>
-            <div className={styles.member}>
+            <div className={styles.member} data-aos="fade-up" data-aos-delay="400">
               <h4>Izabela Wiza</h4>
               <p>Wspólnik</p>
             </div>
